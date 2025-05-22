cbc6098ecf8621f9092c65e00b2a28f0491a2e72
Fausto Leal <fausto.leal2099@gmail.com>
2025-05-22 20:46:55 -0300

    add component intro

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,9 @@
-import { useState } from "react";
+import Intro from "./components/intro/intro";
 
 function App() {
-  const [count, setCount] = useState(3);
-
   return (
     <>
-      <h1>{count}</h1>
-      <button onClick={() => setCount(count + 2)}>{"button"}</button>
+      <Intro />
     </>
   );
 }
---
+++ frontend/src/components/intro/intro.tsx
@@ -0,0 +1,31 @@
+import AutoStoriesIcon from "@mui/icons-material/AutoStories";
+import "./intro.css";
+import { useEffect, useState } from "react";
+
+const Intro = () => {
+  const [isVisible, setIsVisible] = useState(true);
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setIsVisible(false);
+    }, 30000);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, []);
+
+  return (
+    <>
+      {isVisible && (
+        <div className="intro">
+          <div className="intro-title">
+            <h1 className="title">Flight Book </h1>
+            <AutoStoriesIcon style={{ fontSize: "3.2rem", margin: "10px" }} />
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Intro;
