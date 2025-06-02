01dd01a000c7be206f8f6408d0a57518106083d0
Fausto Leal <fausto.leal2099@gmail.com>
2025-06-02 11:46:18 -0300

    add aerodromo component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,13 @@ import Intro from "./components/intro/Intro";
 import HorasForm from "./components/horas_de_vuelo/HorasForm";
 import PilotosForm from "./components/pilotos/PilotosForm";
 import CreateAccount from "./components/pilotos/CreateAccount";
+import AerodromoForm from "./components/aerodromos/aerodromoForm";
 
 function App() {
   return (
     <>
       <Intro />
-      <HorasForm />
+      <AerodromoForm />
     </>
   );
 }
